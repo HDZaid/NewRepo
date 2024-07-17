2bc7e9f5ad01bbc1e9e26a846357727627663628
Henriddiaz <henryddiaz696@gmail.com>
2024-07-17 08:42:42 -0600

    tecer commit

--- EjerciciosGit/EjerciciosGit/Program.cs
+++ EjerciciosGit/EjerciciosGit/Program.cs
@@ -3,11 +3,3 @@ Console.WriteLine("Hello, World!");
 
 
 Console.WriteLine("se hizo algo ");
-Console.WriteLine("nuevo cambio");
-Console.WriteLine("CAMBIOS");
-Console.WriteLine("lkajsdflkjasdflk");
-Console.WriteLine("asdfasfas");
-Console.WriteLine("ALGO NUEVO");
-Console.WriteLine("SADFASDF");
-Console.WriteLine("sadlkfjaslkfjaslkjflk;asjfl;kasjflksadjflkasdjflksadjf");
-Console.WriteLine("alskdjflksdjaflksdjf");
