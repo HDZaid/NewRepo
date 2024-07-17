f9dbbf3216be47316e8a9c3eaa27669fee627614
Henriddiaz <henryddiaz696@gmail.com>
2024-07-17 09:09:19 -0600

    cambio en develop

--- EjerciciosGit/EjerciciosGit/Program.cs
+++ EjerciciosGit/EjerciciosGit/Program.cs
@@ -1,5 +1,12 @@
-﻿
+﻿//PRIMERA SECCION
 Console.WriteLine("Hello, World!");
 
 
 Console.WriteLine("se hizo algo ");
+Console.WriteLine("Jean gray = jenny gris ");
+Console.WriteLine("CAMBIO BY HENRY");
+
+//SEGUNDA SECCION
+
+
+//TECERA SECCION
