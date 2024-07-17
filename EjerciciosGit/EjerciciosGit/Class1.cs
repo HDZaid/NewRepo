b02c8dcc056bca385b10c61673f1db4075c035c3
Henriddiaz <henryddiaz696@gmail.com>
2024-07-16 20:01:53 -0600

    segundo commit

---
+++ EjerciciosGit/EjerciciosGit/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosGit
+{
+    internal class Class1
+    {
+    }
+}
---
+++ EjerciciosGit/EjerciciosGit/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+
+Console.WriteLine("se hizo algo ");
+Console.WriteLine("nuevo cambio");
+Console.WriteLine("CAMBIOS");
+Console.WriteLine("lkajsdflkjasdflk");
+Console.WriteLine("asdfasfas");
+Console.WriteLine("ALGO NUEVO");
+Console.WriteLine("SADFASDF");
+Console.WriteLine("sadlkfjaslkfjaslkjflk;asjfl;kasjflksadjflkasdjflksadjf");
+Console.WriteLine("alskdjflksdjaflksdjf");
